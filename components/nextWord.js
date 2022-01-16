7c339af2e4ca2c3fa2f99687dbc66a35bc80b2ef
Pavel Polivka <pavel.polivka@hey.com>
2022-01-16 16:41:06 +0100

    fixes

--- components/nextWord.js
+++ components/nextWord.js
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import Countdown from 'react-countdown';
 import { nextDate } from '../lib/words';
 
 export const NextWord = () => {
 
     // Random component
-    const Completionist = () => <span><a href="/">Hrej</a></span>;
+    const Completionist = () => <span><Link href="/">Hrej</Link></span>;
 
     // Renderer callback with condition
     const renderer = ({ hours, minutes, seconds, completed }) => {
