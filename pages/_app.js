ff39734b619dd9d9b7c1ae3c44167c9b042c6da8
Pavel Polivka <pavel.polivka@hey.com>
2022-01-16 19:43:43 +0100

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PTKKB6EMQV"></script>
+      <script
+          dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PTKKB6EMQV');
+          `
+          }}
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
